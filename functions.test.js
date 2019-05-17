0a070768b19cf6370bdae35073be7b41c3217dc5
BlueTinHourglass <49942596+BlueTinHourglass@users.noreply.github.com>
2019-05-17 16:09:33 +0800

    Add files via upload

---
+++ functions.test.js
@@ -0,0 +1,106 @@
+const functions = require('./functions');
+
+// CHECK FOR TRUTHY & FALSY VALUES
+// toBeNull matches only toBeNull
+// toBeUndefined matches only undefined
+// toBeTruthy matches anything than an if statement treats as true
+// toBeFalsy matches anything that an if statement treats as false
+
+// beforeEach(() => initDatabase());
+// afterEach(() => closeDatabase());
+
+// beforeAll(() => initDatabase());
+// afterAll(() => closeDatabase());
+
+// const initDatabase = () => console.log('Database Initialized...');
+// const closeDatabase = () => console.log('Database Closed...')
+const nameCheck = () => console.log('Checking Name....');
+
+describe('Checking Names', () => {
+  beforeEach(() => nameCheck());
+
+  test('User is Jeff', () => {
+    const user = 'Jeff';
+    expect(user).toBe('Jeff');
+  });
+
+  test('User is Karen', () => {
+    const user = 'Karen';
+    expect(user).toBe('Karen');
+  });
+});
+
+// toBe
+test('Adds 2+2 to equal 4', () => {
+  expect(functions.add(2, 2)).toBe(4);
+});
+
+// not
+test('Adds 2 + 2 to NOT equal 5', () => {
+  expect(functions.add(2,2)).not.toBe(5);
+});
+
+// toBeNull
+test('Should be null', () => {
+  expect(functions.isNull(2,2)).toBeNull();
+});
+
+// toBeFalsy
+test('Should be falsy', () => {
+  expect(functions.checkValue(0)).toBeFalsy();
+});
+
+test('Should be falsy', () => {
+  expect(functions.checkValue(undefined)).toBeFalsy();
+});
+
+test('Should be falsy', () => {
+  expect(functions.checkValue(null)).toBeFalsy();
+});
+
+test('Should be falsy', () => {
+  expect(functions.checkValue(false)).toBeFalsy();
+});
+
+// toEqual
+test('User should be Brad Traversy object', () => {
+  expect(functions.createUser()).toEqual({
+    firstName: 'Brad',
+    lastName: 'Traversy'
+  });
+});
+
+//Less than and greater than
+test('Should be under 1600', () => {
+  const load1 = 800;
+  const load2 = 700;
+  expect(load1 + load2).toBeLessThan(1600);
+});
+
+// Regex
+test('There is no I in team', () => {
+  expect('team').not.toMatch(/I/i);
+});
+
+// Arrays
+test('Admin should be in usernames', () => {
+  usernames = ['john', 'karen', 'admin'];
+  expect(usernames).toContain('admin');
+});
+
+// Working with async data
+
+// Promise
+test('User fetched name should be Leanne Graham', () => {
+  expect.assertions(1);
+  return functions.fetchUser().then(data => {
+    expect(data.name).toEqual('Leanne Graham');
+  });
+});
+
+// Async Await
+test('User fetched name should be Leanne Graham', async () => {
+  expect.assertions(1);
+  const data = await functions.fetchUser();
+  expect(data.name).toEqual('Leanne Graham');
+});
